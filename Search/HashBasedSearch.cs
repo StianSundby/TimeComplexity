6d6f1613a1aa95c5379416dc0314e41231cbcd6b
StianSundby <88629622+StianSundby@users.noreply.github.com>
2023-10-20 23:12:54 +0200

    Create HashBasedSearch.cs

---
+++ Search/HashBasedSearch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uMethodLib.Search
+{
+    public class HashBasedSearch
+    {
+        /// <summary>
+        /// Performs a Hash-Based search for a target value in the hash table.
+        /// Typically works better on very large datasets.
+        /// </summary>
+        /// <param name="hashTable">Hash table mapping array elements to their indices.</param>
+        /// <param name="target">Target value to find.</param>
+        /// <returns>Index of the target if found, or -1 if the target is not in the hash table.</returns>
+        public static int HashSearch(int[] arr, int target)
+        {
+            var hashTable = ConvertArrayToHashTable(arr);
+            if (hashTable.ContainsKey(target)) 
+                return hashTable[target];
+            return -1;
+        }
+
+        /// <summary>
+        /// Converts an array to a hash table where elements become keys and their indices become values.
+        /// </summary>
+        /// <param name="arr">Array to convert to a hash table.</param>
+        /// <returns>Hash table mapping array elements to their indices.</returns>
+        private static Dictionary<int, int> ConvertArrayToHashTable(int[] arr)
+        {
+            var hashTable = new Dictionary<int, int>();
+
+            for (int i = 0; i < arr.Length; i++)
+                if (!hashTable.ContainsKey(arr[i])) 
+                    hashTable[arr[i]] = i; 
+
+            return hashTable;
+        }
+    }
+}
