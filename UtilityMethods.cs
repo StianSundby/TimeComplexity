1e320d6bb48dbc73b995d2d345956ebd48d1fad6
Stian <88629622+StianSundby@users.noreply.github.com>
2023-10-25 12:19:36 +0200

    Update UtilityMethods.cs

--- UtilityMethods.cs
+++ UtilityMethods.cs
@@ -1,9 +1,11 @@
-﻿using Spectre.Console;
+using Spectre.Console;
 
 namespace uMethodLib
 {
     internal class UtilityMethods
     {
+        static readonly Random random = new();
+
         /// <summary>
         /// Measures the performance of a given action and returns the elapsed time.
         /// </summary>
@@ -12,25 +14,44 @@ namespace uMethodLib
         /// <returns>The elapsed time for the action execution.</returns>
         public static TimeSpan MeasurePerformance<T>(Func<T> action)
         {
-            var watch = System.Diagnostics.Stopwatch.StartNew(); // Start a stopwatch to measure elapsed time
-            T result = action(); // Execute the action
-            watch.Stop(); // Stop the stopwatch
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            T result = action();
+            watch.Stop();
 
-            if (result == null) return TimeSpan.Zero; 
+            if (result == null) 
+                return TimeSpan.Zero;
 
             if (typeof(T) == typeof(bool))
             {
-                bool boolResult = (bool)(object)result; // Unbox the result as a boolean
-                return boolResult ? watch.Elapsed : TimeSpan.Zero; // If the result is true, return the elapsed time; otherwise, return TimeSpan.Zero
+                bool boolResult = (bool)(object)result;
+                return boolResult ? watch.Elapsed : TimeSpan.Zero;
             }
-            else if (typeof(T) == typeof(int))
+            else if (typeof(T) == typeof(int) || typeof(T) == typeof(int[]))
             {
-                int intResult = (int)(object)result; // Unbox the result as an integer
-                return intResult != -1 ? watch.Elapsed : TimeSpan.Zero; // If the result is not -1, return the elapsed time; otherwise, return TimeSpan.Zero
+                if (typeof(T) == typeof(int))
+                {
+                    int intResult = (int)(object)result;
+                    return intResult != -1 ? watch.Elapsed : TimeSpan.Zero;
+                }
+                else
+                {
+                    var time = watch.Elapsed;
+                    var arrayResult = (int[])(object)result;
+                    var sorted = IsSorted(arrayResult);
+                    return sorted ? time : TimeSpan.Zero;
+                }
+            }
+            else if (typeof(T) == typeof(IList<int>))
+            {
+                var time = watch.Elapsed;
+                var arrayResult = (IList<int>)(object)result;
+                var sorted = IsSorted(arrayResult);
+                return sorted ? time : TimeSpan.Zero;
             }
 
-            return TimeSpan.Zero; // Return TimeSpan.Zero for any other type of result
+            return TimeSpan.Zero;
         }
+
 
         /// <summary>
         /// Generates a list of random integers with the specified size.
@@ -39,14 +60,11 @@ namespace uMethodLib
         /// <returns>A list of random integers.</returns>
         public static (List<int> x, List<int> y) GenerateLargeLists(int n)
         {
-            // Generate two large lists of integers, with one common section at random indices
-            Random random = new Random();
             List<int> x = new();
             List<int> commonSublist = new() { 1, 2, 3, 4, 5 };
 
             int commonSublistLength = commonSublist.Count;
             int xCommonSublistIndex = random.Next(0, n - commonSublistLength);
-            int yCommonSublistIndex = random.Next(0, n - commonSublistLength);
 
             for (int i = 0; i < n; i++)
             {
@@ -59,22 +77,6 @@ namespace uMethodLib
             return (commonSublist, x);
         }
 
-        /// <summary>
-        /// Prints the test results to the console.
-        /// </summary>
-        /// <param name="results">A dictionary containing test results with algorithm names as keys and execution times as values.</param>
-        public static void PrintResults(Dictionary<string, TimeSpan> results)
-        {
-            int nameWidth = 40;
-            var sortedResults = results.OrderBy(pair => pair.Value); // Sort the results by execution time
-
-            foreach (var result in sortedResults)
-            {
-                Console.Write($"{result.Key.PadRight(nameWidth)}");
-                string timeString = result.Value == TimeSpan.Zero ? "Not found." : $"{result.Value.TotalMilliseconds:0.00} ms";
-                Console.WriteLine($"{timeString}");
-            }
-        }
 
         public static int GetMin(int x, int y)
         {
@@ -96,25 +98,41 @@ namespace uMethodLib
             var gradient = new Color[count];
             for (int i = 0; i < count; i++)
             {
-                int redValue = (int)(255 * i / (count - 1)); // Gradually increase the red component
+                int redValue = 255 * i / (count - 1); // Gradually increase the red component
                 int greenValue = 255 - redValue; // Gradually decrease the green component
                 gradient[count - 1 - i] = new Color((byte)greenValue, (byte)redValue, 0); // Swap green and red and reverse the order
             }
             return gradient;
         }
 
-        public static int[] GenerateArray(int length, bool sorted)
+        public static int[] GenerateIntArray(int length, bool sorted, int maxVal = 0)
         {
-            Random rnd = new();
             int[] arr = new int[length];
 
             if (sorted)
                 for (int i = 0; i < length; i++)
                     arr[i] = i;
             else
+                if (maxVal == 0)
+                    for (int i = 0; i < length; i++)
+                        arr[i] = random.Next(1, length * 10);
+                else
+                    for (int i = 0; i < length; i++)
+                        arr[i] = random.Next(0, maxVal + 1);
+
+            return arr;
+        }
+
+        public static float[] GenerateFloatArray(int length, bool sorted)
+        {
+            float[] arr = new float[length];
+            if (sorted)
                 for (int i = 0; i < length; i++)
-                    arr[i] = rnd.Next(1, length - 1);
-            
+                    arr[i] = (float)i;
+            else
+                for (int i = 0; i < length; i++)
+                    arr[i] = (float)random.NextDouble() * (length - 1);
+
             return arr;
         }
 
@@ -131,6 +149,36 @@ namespace uMethodLib
 
             Array.Sort(arr);
             return arr;
+        }
+
+        public static int[,] GenerateRandomGraph(int nodeCount, int maxEdgeWeight)
+        {
+            int[,] graph = new int[nodeCount, nodeCount];
+
+            for(int i = 0; i < nodeCount; i++) 
+            {
+                for (int j = 0; j < nodeCount; j++)
+                {
+                    if (i == j)
+                        graph[i, j] = 0;
+                    else
+                        graph[i, j] = random.Next(1, maxEdgeWeight + 1);
+                }
+            }
+            return graph;
+        }
+
+    /// <summary>
+    /// Verify that a list of type integer is sorted in ascending order.
+    /// </summary>
+    /// <param name="arr">Collection to verify</param>
+    /// <returns></returns>
+        private static bool IsSorted(IList<int> arr)
+        {
+            for (int i = 1; i < arr.Count; i++)
+                if (arr[i] < arr[i - 1]) 
+                    return false;
+            return true;
         }
     }
 }
